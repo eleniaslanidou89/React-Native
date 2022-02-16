2a7574e043532b6851404141d94e298fe7d55ef7
Eleni <helena.aslanidou@gmail.com>
2022-02-16 12:41:43 +0100

    Created Buttons
    'Search by City' Button and 'Search by Country' Button

--- citypop-app-react-native/App.js
+++ citypop-app-react-native/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Button, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{ marginTop: '15%', marginBottom: '10%', marginLeft: '40%' }}
+      >
+        CityPop
+      </Text>
       <StatusBar style="auto" />
+      <Button
+        // onPress={}
+        title="SEARCH BY CITY"
+        color="#841584"
+      />
+      <Button
+        // onPress={}
+        title="SEARCH BY COUNTRY"
+        color="#841584"
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 90,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-});
+  searchbar: {
+    marginTop: 20,
+    height: 50,
+    borderRadius: 20,
+    backgroundColor: 'gray',
+    width: '100%',
+    paddingHorizontal: '1%',
+  },
+})
