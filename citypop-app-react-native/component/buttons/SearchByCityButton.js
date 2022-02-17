98a30f54d5fa4d4ae6eb6be2045ea64fed937726
Eleni <helena.aslanidou@gmail.com>
2022-02-17 11:02:18 +0100

    Created components for Buttons

---
+++ citypop-app-react-native/component/buttons/SearchByCityButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Button } from 'react-native'
+
+export default function SearchByCityTitle() {
+  return (
+    <View style={styles.container}>
+      <Button
+        // onPress={}
+        title="SEARCH BY CITY"
+        color="#841584"
+      />
+    </View>
+  )
+}
---
+++ citypop-app-react-native/component/titles/HomePageTitle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function SearchByCityTitle() {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{ marginTop: '15%', marginBottom: '10%', marginLeft: '40%' }}
+      >
+        CityPop
+      </Text>
+    </View>
+  )
+}
