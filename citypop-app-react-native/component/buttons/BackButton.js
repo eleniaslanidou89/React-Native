5fab004d0870c4897d511de5f8f7a9ec16080dd5
Eleni <helena.aslanidou@gmail.com>
2022-02-18 10:13:56 +0100

    Back Button component

---
+++ citypop-app-react-native/component/buttons/BackButton.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Button } from 'react-native'
+
+export default function BackButton({ navigation }) {
+
+  const pressHandlerBack = () => {
+    navigation.goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button
+        onPress={pressHandlerBack}
+        title="CityPop"
+        color="#841584"
+      />
+    </View>
+  )
+}
